1f3be61f85de303771ba44870d96c1952e69fc94
Christos Kontomitros <christos.kontomitros@upstreamsystems.com>
2023-02-03 22:45:09 +0200

    pwa try

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -9,7 +9,25 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: `src/images/icon.png`,
+        crossOrigin: `use-credentials`,
+      },
+      
+    },
+     `gatsby-plugin-offline`,
+      
+    
+  ],
 }
 
 export default config
